aa3e304d39f94fffeabe13bf424874a41527755c
jasmine00716 <sumin00716@gmail.com>
2022-05-18 20:37:18 +0900

    CourseController 중간 커밋

--- coursemos_spring_boot/src/main/java/com/example/demo/CourseController.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/CourseController.java
@@ -1,5 +1,24 @@
 package com.example.demo;
 
-public class CourseController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping("/course/writeCourse")
+public class CourseController {
+	@GetMapping
+	public String writeCourseForm() {
+		return "course/writeCourse";
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public String writeCourse(@RequestBody String body) {
+		System.out.println("Request Body: " + body);
+		return body;
+	}
 }
