14d5d286874f20176b699c7b5e84c3e65ca9ab93
cnsejr2 <cnsejr345@gmail.com>
2022-05-19 01:41:15 +0900

    search

--- coursemos_spring_boot/src/main/java/com/example/demo/SearchPlaceController.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/SearchPlaceController.java
@@ -1,11 +1,26 @@
 package com.example.demo;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@RestController
 @RequestMapping("/course")
 public class SearchPlaceController {
 	@GetMapping("/search")
-	public String Form() {
-		return "search/searchPlace";
+	public ModelAndView searchPlaceForm() {
+		
+		ModelAndView mav = new ModelAndView("search"); // view 이름: "hello" 
+		return mav;
+	}
+	@PostMapping("/serach")
+	public ModelAndView submit() {
+		ModelAndView mav = new ModelAndView("searchResult"); // view 이름: "hello"      
+	   return mav;   
 	}
 }
