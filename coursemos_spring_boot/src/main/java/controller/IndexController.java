02192c77d4081993fdb44e0303e624a0a0741172
20190937 <20190937@dongduk.ac.kr>
2022-05-18 22:09:14 +0900

    spring jsp 연결 설정

---
+++ coursemos_spring_boot/src/main/java/controller/IndexController.java
@@ -0,0 +1,13 @@
+package controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/test")
+	public String index(Model model) {
+		model.addAttribute("greeting","greeting");
+		return "test";
+	}
+}
