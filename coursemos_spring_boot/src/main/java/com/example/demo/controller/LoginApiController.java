8d496bc298caf32a604dc3015c3856dfd0ddcbfe
leemj <20190988@dongduk.ac.kr>
2022-05-25 21:09:20 +0900

    login api 주석

--- coursemos_spring_boot/src/main/java/com/example/demo/controller/LoginApiController.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/controller/LoginApiController.java
@@ -1,18 +1,18 @@
-package com.example.demo.controller;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public class LoginApiController {
-	@Value("${KAKAO.JS}")
-	String kakaoApi;
-	
-	@RequestMapping(value="/getKakaoApi.do", method=RequestMethod.PUT)
-	public String getKakaoApi() {
-		return kakaoApi;
-	}
-}
+//package com.example.demo.controller;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class LoginApiController {
+//	@Value("${KAKAO.JS}")
+//	String kakaoApi;
+//	
+//	@RequestMapping(value="/getKakaoApi.do", method=RequestMethod.PUT)
+//	public String getKakaoApi() {
+//		return kakaoApi;
+//	}
+//}
