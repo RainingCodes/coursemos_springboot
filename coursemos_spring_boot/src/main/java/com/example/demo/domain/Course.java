1370b9588037ab9554520fcc8e753b2b6e6cb074
cnsejr2 <cnsejr345@gmail.com>
2022-05-29 14:15:42 +0900

    domain course 수정

--- coursemos_spring_boot/src/main/java/com/example/demo/domain/Course.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/domain/Course.java
@@ -1,23 +1,49 @@
 package com.example.demo.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @SuppressWarnings("serial")
 public class Course implements Serializable {
 
 	private int courseId;
+	private String courseName;
 	private String courseContents;
-	private String coordinates; //좌표
-	private int travelTime; // 소요시간
-	private int userId;
-	private int[] storeId;
-	private String taste; // 취향
+	private String[] coordinates; //좌표
+	private int travelTime;
+	private int memberId;
+	private String taste;
+	private String photo;
+	private int likes;
+	private Date writtenDate;
 	
+	
+	
+	public Course(int courseId, String courseName, String courseContents, String[] coordinates, int travelTime,
+			int memberId, String taste, String photo, int likes, Date writtenDate) {
+		super();
+		this.courseId = courseId;
+		this.courseName = courseName;
+		this.courseContents = courseContents;
+		this.coordinates = coordinates;
+		this.travelTime = travelTime;
+		this.memberId = memberId;
+		this.taste = taste;
+		this.photo = photo;
+		this.likes = likes;
+		this.writtenDate = writtenDate;
+	}
 	public int getCourseId() {
 		return courseId;
 	}
 	public void setCourseId(int courseId) {
 		this.courseId = courseId;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
 	}
 	public String getCourseContents() {
 		return courseContents;
@@ -25,29 +51,23 @@ public class Course implements Serializable {
 	public void setCourseContents(String courseContents) {
 		this.courseContents = courseContents;
 	}
-	public String getCoordinates() {
+	public String[] getCoordinates() {
 		return coordinates;
 	}
-	public void setCoordinates(String coordinates) {
+	public void setCoordinates(String[] coordinates) {
 		this.coordinates = coordinates;
 	}
 	public int getTravelTime() {
 		return travelTime;
 	}
-	public void setTravelTime(int time) {
-		this.travelTime = time;
+	public void setTravelTime(int travelTime) {
+		this.travelTime = travelTime;
 	}
-	public int getUserId() {
-		return userId;
+	public int getMemberId() {
+		return memberId;
 	}
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	public int[] getStoreId() {
-		return storeId;
-	}
-	public void setStoreId(int[] storeId) {
-		this.storeId = storeId;
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
 	}
 	public String getTaste() {
 		return taste;
@@ -55,28 +75,26 @@ public class Course implements Serializable {
 	public void setTaste(String taste) {
 		this.taste = taste;
 	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public Date getWrittenDate() {
+		return writtenDate;
+	}
+	public void setWrittenDate(Date writtenDate) {
+		this.writtenDate = writtenDate;
+	} 
 	
-	public Course(int courseId, String courseContents, String coordinates, int time) {
-		super();
-		this.courseId = courseId;
-		this.courseContents = courseContents;
-		this.coordinates = coordinates;
-		this.travelTime = time;
-	}
 	
-	public Course(int courseId, String courseContents, String coordinates, int time, int userId, int[] storeId,
-			String taste) {
-		super();
-		this.courseId = courseId;
-		this.courseContents = courseContents;
-		this.coordinates = coordinates;
-		this.travelTime = time;
-		this.userId = userId;
-		this.storeId = storeId;
-		this.taste = taste;
-	}
-	public Course() {
-		// TODO Auto-generated constructor stub
-	}
+
 
 }
