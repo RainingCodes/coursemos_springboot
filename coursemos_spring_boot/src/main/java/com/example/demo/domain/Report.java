be26643019cf545dfb1064a6597b60659089fbea
20190937 <20190937@dongduk.ac.kr>
2022-06-07 10:35:02 +0900

    jpa

--- coursemos_spring_boot/src/main/java/com/example/demo/domain/Report.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/domain/Report.java
@@ -1,15 +1,22 @@
 package com.example.demo.domain;
 
 import java.util.Date;
+
+import javax.persistence.*;
 import lombok.*;
 
-@Getter @Setter @ToString
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor 
 public class Report {
+	@Id
 	private int reportId;
+	private int userId;
+	
+	@Column(name="category")
+	private String reportCategory;
 	private Date writtenDate;
+	private boolean state;
+	
 	private int courseId;
 	private int reviewId;
-	private int userId;
-	private String reportCategory;
-	private boolean state;
 }
