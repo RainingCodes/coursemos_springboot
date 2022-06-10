178e897ddf0c0a7e1b9fdfbbe13700724c8b78bb
jasmine00716 <sumin00716@gmail.com>
2022-06-10 19:18:25 +0900

    CourseDao.java 주석 지우고 재커밋

--- coursemos_spring_boot/src/main/java/com/example/demo/dao/CourseDao.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/dao/CourseDao.java
@@ -14,31 +14,22 @@ public interface CourseDao {
 
 	void deleteCourse(Course course) throws DataAccessException;
 	
-	// ��Ұ� �� ���� �Է� �� ���
 	List<Course> searchCourseListByPlace(String place) throws DataAccessException;
 
-	// ���⿡ ����
 	List<Course> getCourseListByTaste(String taste) throws DataAccessException;
 
-	// ���ƿ� ���� ����
 	List<Course> getCourseListByLike(int like) throws DataAccessException;
 
-	// �̵��Ÿ��� ����
 	List<Course> getCourseListByTime(int time) throws DataAccessException;
 	
-	// ��Ұ� �� ���� �Է� �� ���
 	List<Course> searchCourseListByKeyword(String keyword) throws DataAccessException;
 
-	// ��Ұ� �� �� �Էµ� ��� + �߰� �Ÿ� ���� ���ϱ�
 	List<Course> searchCourseListByPlace(String place1, String place2) throws DataAccessException;
 
-	// ���⿡ ����
 	List<Course> getCourseListByTaste(String place1, String place2, String taste) throws DataAccessException;
 
-	// ���ƿ� ���� ����
 	List<Course> getCourseListByLike(String place1, String place2) throws DataAccessException;
 
-	// �̵��Ÿ��� ����
 	List<Course> getCourseListByTime(String place1, String place2) throws DataAccessException;	
 	
 	
