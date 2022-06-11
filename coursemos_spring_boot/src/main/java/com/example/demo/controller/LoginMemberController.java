e1956a435952fa03a2f74f457365f4b1c4ef84b1
leemj <20190988@dongduk.ac.kr>
2022-06-11 14:09:22 +0900

    login member controller

--- coursemos_spring_boot/src/main/java/com/example/demo/controller/LoginMemberController.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/controller/LoginMemberController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+import com.example.demo.domain.Member;
 import com.example.demo.domain.SessionMember;
+import com.example.demo.service.MemberService;
+import com.example.demo.validator.LoginValidator;
+import com.example.demo.validator.MemberValidator;
 
 
 
@@ -18,11 +24,15 @@ import com.example.demo.domain.SessionMember;
 @SessionAttributes("sessionMember")
 @RequestMapping("/login2")
 public class LoginMemberController {
+	@Autowired
+	private MemberService memberService;
+	
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String form() {
 		return "member/login2";
 	}
+	
 	@ModelAttribute("sessionMember")
 	public SessionMember formBacking(HttpServletRequest request) {
 		SessionMember sessionMember = new SessionMember(); // MemberInfo 객체 생성
@@ -30,13 +40,29 @@ public class LoginMemberController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String submit(@ModelAttribute SessionMember member,
+	public String login(@Valid @ModelAttribute SessionMember sessionMember,
 			BindingResult result, SessionStatus status) {
+				Member member = memberService.findMemberByNickName2(sessionMember.getNickName());
+				if(sessionMember.getNickName() != null && sessionMember.getPassword() != null) {
+					if(member == null)
+						sessionMember.setCheck1(false);
+					else if(!member.getPassword().equals(sessionMember.getPassword())) {
+						sessionMember.setCheck1(true);
+						sessionMember.setCheck2(false);
+					}
+						
+					else {
+						sessionMember.setCheck1(true);
+						sessionMember.setCheck2(true);
+						sessionMember.setPoints(member.getPoints());
+						sessionMember.setTasteCode(member.getTasteCode());
+					}
+				}
+				new LoginValidator().validate(sessionMember, result);
 				if (result.hasErrors()) {
 					return "member/login2";
 				}
-				System.out.println(member);
-				status.setComplete();
 				return "index"; // “member” 객체가 view에 전달됨 (request를 통해) 
 			}
+	
 }
