030ba73449b380466b73aeb2bb8ac3b50f253f6d
jasmine00716 <sumin00716@gmail.com>
2022-06-11 16:27:04 +0900

    registerForm 값 view 화면에 출력

--- coursemos_spring_boot/src/main/java/com/example/demo/controller/CourseController.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/controller/CourseController.java
@@ -1,27 +1,24 @@
 package com.example.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.domain.Course;
-import com.example.demo.domain.SessionCourse;
 import com.example.demo.service.CourseService;
 import com.example.demo.validator.CourseValidator;
 
 @Controller
 @SessionAttributes("course")
+@RequestMapping("/course")
 public class CourseController {
 	
 	@Autowired
@@ -33,43 +30,77 @@ public class CourseController {
 		return course;
 	}
 	
-	ModelAndView mav;
-	
-	@RequestMapping("course/registerForm")
-	public ModelAndView writeCourse() {
-		mav = new ModelAndView("/course/registerForm");
+	@RequestMapping("registerForm")
+	public ModelAndView init() {
+		ModelAndView mav = new ModelAndView("/course/registerForm");
 		return mav;
 	}
 	
-//	@RequestMapping(method=RequestMethod.GET)
-//	public String form() {
-//		return "course/registerForm";
-//	}
-	
-	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(value="/view/1", method=RequestMethod.POST)
 	public String submit(@ModelAttribute Course course, BindingResult result, SessionStatus status, HttpServletRequest request) {
 		new CourseValidator().validate(course, result);
-		
 		if (result.hasErrors()) {
+			System.out.println("Validation Error !!!");
 			return "course/registerForm";
 		}
-		
-		courseService.insertCourse(course);
-		SessionCourse sessionCourse = new SessionCourse(course.getCourseId(), course.getCourseName(), course.getCourseContents(), course.getMemberId(), course.getTaste(), course.getLikes(), course.getWrittenDate(), course.getPlaceId1(), course.getPlaceId2(), course.getPlaceId3());
-		status.setComplete();
-		HttpSession session = request.getSession();
-		session.setAttribute("courseSession", sessionCourse);
-		return "view/1";
-	}
 
-
-	@RequestMapping("course/view/{courseId}")
-	public ModelAndView viewCourse() {
-		//courseId로 코스 상세정보 불러와서 화면에 출력
+		System.out.println("courseName은 " +request.getParameter("courseName"));
 		
-			
+		System.out.println(course.toString());
 		
-		mav = new ModelAndView("/course/view");
-		return mav;
+		//courseService.insertCourse(course);
+//		
+//		SessionCourse sessionCourse = new SessionCourse(course.getCourseId(), course.getCourseName(), course.getCourseContents(), course.getMemberId(), course.getTaste(), course.getLikes(), course.getWrittenDate(), course.getPlaceId1(), course.getPlaceId2(), course.getPlaceId3());
+//		status.setComplete();
+//		
+//		HttpSession session = request.getSession();
+//		session.setAttribute("courseSession", sessionCourse);
+//		
+		System.out.println("submit complete!!!");
+		
+		return "/course/view";
 	}
+	
+
+//	
+//	@Autowired
+//	private CourseService courseService;
+//	
+
+//	
+//	ModelAndView mav;
+//	
+//	@RequestMapping("registerForm")
+//	public ModelAndView writeCourse() {
+//		mav = new ModelAndView("/course/registerForm");
+//		return mav;
+//	}
+//	
+//	@RequestMapping(method=RequestMethod.POST)
+//	public String submit(@ModelAttribute Course course, BindingResult result, SessionStatus status, HttpServletRequest request) {
+//		new CourseValidator().validate(course, result);
+//		
+//		if (result.hasErrors()) {
+//			return "course/registerForm";
+//		}
+//		
+//		courseService.insertCourse(course);
+//		SessionCourse sessionCourse = new SessionCourse(course.getCourseId(), course.getCourseName(), course.getCourseContents(), course.getMemberId(), course.getTaste(), course.getLikes(), course.getWrittenDate(), course.getPlaceId1(), course.getPlaceId2(), course.getPlaceId3());
+//		status.setComplete();
+//		HttpSession session = request.getSession();
+//		session.setAttribute("courseSession", sessionCourse);
+//		return "view/1";
+//	}
+//
+//
+//	@RequestMapping("view/{courseId}")
+//	public ModelAndView viewCourse() {
+//		//courseId로 코스 상세정보 불러와서 화면에 출력
+//		
+//			
+//		
+//		mav = new ModelAndView("/course/view");
+//		return mav;
+//	}
+	
 }
--- coursemos_spring_boot/src/main/webapp/WEB-INF/jsp/course/view.jsp
+++ coursemos_spring_boot/src/main/webapp/WEB-INF/jsp/course/view.jsp
@@ -17,8 +17,8 @@
 	String roadAddress3 = request.getParameter("road_address3") == null ? "": request.getParameter("road_address3");
 	String roadAddressList[] = {roadAddress1, roadAddress2, roadAddress3};
 	
+	String writtenDate = request.getParameter("writtenDate");
 	String category = request.getParameter("category");
-	
 	String contents = request.getParameter("contents");
 %>    
 <!DOCTYPE html>
@@ -82,17 +82,18 @@
 					<header class="mb-4">
 						<h1 style="text-align: center;">코스 상세페이지</h1>
 					</header>
+					<div style="margin-left:400px;">작성자: ${course.memberId}, 작성일: ${course.writtenDate }</div>
 					<div>
 					<p></p>
 					<!-- Post title-->
-					<h4 style="font-weight:bold;" id="courseTitle"><%=inputTitle %></h4><br>
+					<h4 style="font-weight:bold;" id="courseTitle">${course.courseName }</h4><br>
 					<!-- <input id="courseTitle" type="text" value="제목"> -->
 					<div class="upload"></div>
 					<h5 style="font-weight:bold;">코스의 분위기</h5>
 					<!-- <div>활동적인, 즐거운</div> -->
-					<div><%=category %></div>
+					<div>${course.taste }</div>
 					<br>
-					<br>
+					<br>					
 					<h5 style="font-weight:bold;">코스 경로 살펴보기</h5>
 					<br>
 					<table>
@@ -224,7 +225,7 @@
 					<h5 style="font-weight:bold;">코스에 대한 설명</h5>
 					<table class="form-control" style="height:200px; border-radius: 5px;">
 						<tr>
-							<td><%=contents%></td>
+							<td>${course.courseContents }</td>
 						</tr>
 					</table>
 					<br>
@@ -275,7 +276,7 @@
 					</script>
 
 					<img style="width:10%; height:10%;" src="../../img/like.png" />
-					<img style="width:10%; height:10%;'" src="../../img/scrap.png"/>
+					<img style="width:9%; height:9%;" src="../../img/scrap.png"/>
 					<!-- <button type="submit" style="width:20%; height:60px; border:none; border-radius:5px; text-align: center; background-color:#0d6efd; color: white;">스크랩</button> -->
 					<!-- 추후 비활성화/활성화 조건 적용하기 -->
 					<button type="submit" style="width:15%; height:60px; border:none; border-radius:5px; text-align: center; background-color:#ced4da;">쿠폰 발급</button>
