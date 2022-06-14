fa87c0334c5f4e6c992aef7192a8e7a5c2b016cb
20190937 <20190937@dongduk.ac.kr>
2022-06-14 18:12:15 +0900

    MemberCouponButton 추가

--- coursemos_spring_boot/src/main/java/com/example/demo/controller/CourseController.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/controller/CourseController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +17,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.domain.Company;
+import com.example.demo.domain.Coupon;
 import com.example.demo.domain.Course;
 import com.example.demo.domain.Place;
+import com.example.demo.service.CompanyService;
+import com.example.demo.service.CouponService;
 import com.example.demo.service.CourseService;
 import com.example.demo.service.PlaceService;
 import com.example.demo.validator.CourseValidator;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Controller
 @SessionAttributes("course")
@@ -29,6 +41,18 @@ public class CourseController {
 	private CourseService courseService;
 	@Autowired
 	private PlaceService placeService;
+	
+	//MemberCouponButton
+	@Autowired
+	private CouponService couponService;
+	@Autowired
+	private CompanyService companyService;
+	@Getter @Setter @AllArgsConstructor @ToString
+	public class MemberCouponButton {
+		private String placeName;
+		private Coupon coupon;
+	}
+	
 	
 	@ModelAttribute("course")
 	public Course formBacking(HttpServletRequest request) {
@@ -88,6 +112,26 @@ public class CourseController {
 		System.out.println("=========view Detail=========");
 		System.out.println(viewCourse.toString() + "\n");
 		System.out.println("=============================");
+		
+		
+		//MemberCouponButton
+		ArrayList<MemberCouponButton> mcb = new ArrayList<MemberCouponButton>();
+						
+		Place[] arr = {course.getPlace1(), course.getPlace2(), course.getPlace3()};
+		for (Place p : arr) {
+			if (p != null) {
+				int id = p.getPlaceId();
+				Company com = companyService.getCompanyByPlaceId(id);
+								
+				if (com != null) {
+					List<Coupon> cou = couponService.getCouponByCompanyId(com.getCompanyId());
+					for (Coupon c : cou) {
+						mcb.add(new MemberCouponButton(com.getPlace().getPlaceName(), c));
+					}
+				}			
+			}
+		}		
+		mav.addObject("couponList", mcb);
 		
 		return mav;
 	}
