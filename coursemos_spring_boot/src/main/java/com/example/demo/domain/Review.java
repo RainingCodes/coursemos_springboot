284175e1135eb2a6696e0f72e1b783b05465ded4
leemj <20190988@dongduk.ac.kr>
2022-06-15 01:07:57 +0900

    domain Review

--- coursemos_spring_boot/src/main/java/com/example/demo/domain/Review.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/domain/Review.java
@@ -1,72 +1,56 @@
 package com.example.demo.domain;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.sql.Date;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@SequenceGenerator(
+		  name = "REVIEW_SEQ_GENERATOR", 
+		  sequenceName = "REVIEW_SEQ", // 매핑할 데이터베이스 시퀀스 이름 
+		  initialValue = 1,
+		  allocationSize = 1)
 @SuppressWarnings("serial")
+@AllArgsConstructor @Getter @Setter @ToString
 public class Review implements Serializable {
-
-	private int reviewId;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "REVIEW_SEQ_GENERATOR")
+	@Id @Column @Getter
+	private Long reviewId;
+	@Column(nullable=false) @Setter @Getter
 	private String reviewContents;
+	@Transient
+	private MultipartFile photo;
+	@Column  @Setter @Getter
 	private String photos;
+	@Column(nullable=false) @Setter @Getter
 	private Date writtenDate;
+	@Column(nullable=false) @Setter @Getter
 	private int likes;
-	private int userId;
-	
-	public int getReviewId() {
-		return reviewId;
-	}
-	public void setReviewId(int reviewId) {
-		this.reviewId = reviewId;
-	}
-
-	
-	public String getReviewContents() {
-		return reviewContents;
-	}
-	public void setReviewContents(String reviewContents) {
-		this.reviewContents = reviewContents;
-	}
-	public String getPhotos() {
-		return photos;
-	}
-	public void setPhotos(String photos) {
-		this.photos = photos;
-	}
-	public Date getWrittenDate() {
-		return writtenDate;
-	}
-	public void setWrittenDate(Date writtenDate) {
-		this.writtenDate = writtenDate;
-	}
-	public int getLikes() {
-		return likes;
-	}
-	public void setLikes(int likes) {
-		this.likes = likes;
-	}
-	public int getUserId() {
-		return userId;
-	}
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	public Review(String reviewContents, String photos, Date writtenDate, int userId) {
-		super();
-		this.reviewContents = reviewContents;
-		this.photos = photos;
-		this.writtenDate = writtenDate;
-		likes = 0;
-		this.userId = userId;
-	}
-	
-	public Review(String reviewContents, Date writtenDate, int userId) {
-		super();
-		this.reviewContents = reviewContents;
-		this.photos = null;
-		this.writtenDate = writtenDate;
-		likes = 0;
-		this.userId = userId;
+	@Column(nullable=false) @Setter @Getter
+	private Long memberId;
+	@Column(nullable=false) @Setter @Getter
+	private Long courseId;
+	@Transient
+	private String nickName;
+	@Transient
+	private String courseName;
+	public Review() {
+		this.likes = 0;
 	}
 
 }
