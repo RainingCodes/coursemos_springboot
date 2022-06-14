e3a503d01f867c65f9d111e5e9896d7b70ccfb9e
20190937 <20190937@dongduk.ac.kr>
2022-06-14 10:43:19 +0900

    company-place 수정 및 연관 연결

--- coursemos_spring_boot/src/main/java/com/example/demo/domain/Company.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/domain/Company.java
@@ -2,9 +2,11 @@ package com.example.demo.domain;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -17,10 +19,13 @@ import lombok.ToString;
 @Entity
 @Setter @Getter @ToString @AllArgsConstructor @NoArgsConstructor 
 public class Company {
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "placeId")
+	private Place place;
 	@Id
 	private Integer companyId;
 	private Integer memberId;
-	private String companyName;
 	private String phone;
 	private String address;
 	@DateTimeFormat(pattern="yyyy-MM-dd")
