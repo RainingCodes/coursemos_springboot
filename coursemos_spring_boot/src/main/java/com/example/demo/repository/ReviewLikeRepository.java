74342aa645e3b4a0ba7c5c51638abcdd7fa07628
leemj <20190988@dongduk.ac.kr>
2022-06-17 09:57:27 +0900

    reviewlikerepository

---
+++ coursemos_spring_boot/src/main/java/com/example/demo/repository/ReviewLikeRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.domain.ReviewLike;
+
+public interface ReviewLikeRepository extends CrudRepository<ReviewLike, String>{
+	List<ReviewLike> findReviewLikeByMemberId(Long memberId);
+	ReviewLike findReviewLikeByReviewIdAndMemberId(Long reviewId, Long memberId);
+}
+
