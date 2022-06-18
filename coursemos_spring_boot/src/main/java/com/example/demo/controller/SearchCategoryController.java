bdf987a6a1bfe145193e4953e500c1019b04ff11
20190937 <20190937@dongduk.ac.kr>
2022-06-18 21:25:13 +0900

    서치 수정

--- coursemos_spring_boot/src/main/java/com/example/demo/controller/SearchCategoryController.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/controller/SearchCategoryController.java
@@ -3,65 +3,33 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.support.PagedListHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.controller.SearchPlaceController.Taste;
-import com.example.demo.dao.CourseDao;
 import com.example.demo.domain.Course;
-import com.example.demo.domain.CourseList;
-import com.example.demo.domain.TasteCategory;
 import com.example.demo.service.CourseService;
-import com.example.demo.service.SearchService;
-import com.example.demo.service.TasteService;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
 @Controller
-//@SessionAttributes({"tList", "cList"})
 public class SearchCategoryController {
 	private static final String SEARCH_DETAIL_VIEW = "detailSearch";
-	private SearchService searchService;
-
-	@Autowired
-	public void setSerachService(SearchService searchService) {
-		this.searchService = searchService;
-	}
-	
-	@Autowired
-	private TasteService tasteService;
-	public void setTasteService(TasteService tasteService) {
-		this.tasteService = tasteService;
-	}
 	
 	@Autowired
 	private CourseService courseService;
-	public void setCourseService(CourseService courseService) {
-		this.courseService = courseService;
-	}
+	
 	@Getter @Setter @AllArgsConstructor @ToString
 	public class Taste {
 		private String code;
@@ -85,10 +53,10 @@ public class SearchCategoryController {
 			@RequestParam("subway") String subway,
 			@RequestParam("taste") String taste,
 			ModelMap model,
-			@PageableDefault(size = 3, sort = "courseId",  direction = Sort.Direction.DESC) Pageable pageable
+			@PageableDefault(size = 3, sort = "courseId",  direction = Sort.Direction.ASC) Pageable pageable
 			) throws Exception {
 		List<Taste> tList = referenceData();
-		//PagedListHolder<Course> cList;
+		
 		Page<Course> cList = null;
 		if (subway == "") {
 			cList = courseService.findCourse1(taste, pageable);
@@ -101,51 +69,5 @@ public class SearchCategoryController {
 		model.put("cList", cList);
 		return SEARCH_DETAIL_VIEW;
 	}
-/*
-	@RequestMapping("/course/detailedSearch2.do")
-	public String handleRequest2(
-			@RequestParam("page") String page,
-			@ModelAttribute("tList") List<TasteCategory> tList,
-			@ModelAttribute("cList") PagedListHolder<Course> cList,
-			BindingResult result) throws Exception {
-		if (tList == null || cList == null) {
-			throw new IllegalStateException("Cannot find pre-loaded category and product list");
-		}
-		if ("next".equals(page)) { cList.nextPage(); }
-		else if ("previous".equals(page)) { cList.previousPage(); }
-		return SEARCH_DETAIL_VIEW;
-	}
-	*/
-	/*
-	@PostMapping
-	public ModelAndView submit(HttpServletRequest request,
-			@RequestParam(value="station", required=false) String station,
-			@RequestParam(value="taste", required=false) String taste,
-			@RequestParam(value="page", required=false) String page,
-			@ModelAttribute("sessionCourse") CourseDao course) throws Exception {
-			System.out.println(taste);
-			ModelAndView mav = new ModelAndView(SEARCH_VIEW); // view 이름: "hello"  
-			//List<Course> cList = searchService.getCourseListByTaste(taste);
-			//mav.addObject("cList", cList);
-			List<TasteCategory> tList = tasteService.getCategory();
-			mav.addObject("tList", tList);
-			handleRequest(page, course, station);
-		   return mav;   
-		
-	}*/
-	/*
-	@RequestMapping("/course/detailedSearch")
-	public String viewCart(
-			HttpServletRequest request,
-			@RequestParam("page") String page,
-			@ModelAttribute("category") TasteCategory category,
-			@ModelAttribute("courseList") PagedListHolder<Course> courseList,
-			BindingResult result) throws Exception {
-		if ("next".equals(page)) { courseList.nextPage(); }
-		else if ("previous".equals(page)) { courseList.previousPage(); }
-		return SEARCH_DETAIL_VIEW;
-	}*/
-	
-	
 
 }
--- coursemos_spring_boot/src/main/java/com/example/demo/controller/SearchPlaceController.java
+++ coursemos_spring_boot/src/main/java/com/example/demo/controller/SearchPlaceController.java
@@ -2,49 +2,20 @@ package com.example.demo.controller;
 
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.support.PagedListHolder;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Order;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.controller.CompanyController.Taste;
 import com.example.demo.domain.Course;
-import com.example.demo.domain.Place;
-import com.example.demo.domain.SessionMember;
-import com.example.demo.domain.TasteCategory;
-import com.example.demo.repository.CourseRepository;
-import com.example.demo.repository.SearchRepository;
 import com.example.demo.service.CourseService;
-import com.example.demo.service.SearchService;
-import com.example.demo.service.TasteService;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -52,29 +23,11 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Controller
-//@SessionAttributes({"tList", "cList"})
 public class SearchPlaceController {
 	private static final String SEARCH_VIEW = "search";
-	private static final String[] method = null;
-//	private static final String RESULT_VIEW = "member/registered";
-	
-	@Autowired
-	private SearchService searchService;
-	public void setSearchService(SearchService searchService) {
-		this.searchService = searchService;
-	}
 	
 	@Autowired
 	private CourseService courseService;
-	public void setCourseService(CourseService courseService) {
-		this.courseService = courseService;
-	}
-	
-	@Autowired
-	private TasteService tasteService;
-	public void setTasteService(TasteService tasteService) {
-		this.tasteService = tasteService;
-	}
 	
 	@Getter @Setter @AllArgsConstructor @ToString
 	public class Taste {
@@ -103,147 +56,24 @@ public class SearchPlaceController {
 		
 		return SEARCH_VIEW;
 	}
+	
 	@RequestMapping("/course/search.do")
 	public String handleRequest(
 			@RequestParam("subway") String subway,
 			@RequestParam("x") double x,
 			@RequestParam("y") double y,
 			ModelMap model
-			,@PageableDefault(size = 3, sort = "courseId",  direction = Sort.Direction.DESC) Pageable pageable
+			,@PageableDefault(size = 3, sort = "courseId",  direction = Sort.Direction.ASC) Pageable pageable
 			) throws Exception {
 		List<Taste> tList = referenceData();
 		model.put("tList", tList);
-//		Sort s;
-//		if (sort.equals("writtenDateR")) {
-//			s = Sort.by("writtenDate").ascending();
-//		} else {
-//			s = Sort.by(sort).descending();
-//		}
-//		pageable = PageRequest.of(-1, 3, s); 
+		
 		Page<Course> cList = courseService.findByPlace1Subway(subway, pageable);
 		model.put("cList", cList);
 		model.put("subway", subway);
 		model.put("x", x);
 		model.put("y", y);
 		
-		return SEARCH_VIEW;
+		return "search_result";
 	}
-//	@RequestMapping("/course/Sort.do")
-//	public String handleRequestSort(
-//			@RequestParam("subway") String subway,
-//			@RequestParam("x") double x,
-//			@RequestParam("y") double y,
-//			@RequestParam(value="sort", required=false, defaultValue="courseId") String sort,
-//			ModelMap model
-//			,@PageableDefault(size = 3, sort = "courseId",  direction = Sort.Direction.DESC) Pageable pageable
-//			) throws Exception {
-//		List<Taste> tList = referenceData();
-//		model.put("tList", tList);
-//		Page<Course> cList = courseService.findByPlace1SubwayOrderByLikes(subway, pageable);
-////		if (sort.equals("writtenDateR")) {
-////			cList = courseService.findCourseWR(subway, pageable);
-////		} else if (sort.equals("likes")){
-////			cList = courseService.findCourseL(subway, pageable);
-////		} else {
-////			cList = courseService.findCourseW(subway, pageable);
-////		}
-		
-		
-//		model.put("cList", cList);
-//		model.put("subway", subway);
-//		model.put("x", x);
-//		model.put("y", y);
-//		
-//		return SEARCH_VIEW;
-//	}
-	/*
-	@RequestMapping("/course/search.do")
-	public String handleRequest(
-			@RequestParam("subway") String subway,
-			@RequestParam("x") double x,
-			@RequestParam("y") double y,
-			ModelMap model
-			) throws Exception {
-		List<Taste> tList = referenceData();
-		//String station = "seoul";
-		PagedListHolder<Course> cList = new PagedListHolder<Course>(this.searchService.getCourseList(subway));
-		cList.setPageSize(4);
-		model.put("subway", subway);
-		model.put("x", x);
-		model.put("y", y);
-		model.put("tList", tList);
-		Sort sort = Sort.by("courseId").descending();
-		//Pageable pageReq = PageRequest.of(4, 5, sort);
-		//Page<Course> cList = courseService.findAll(pageable);
-		model.put("cList", cList);
-		
-		return SEARCH_VIEW;
-	}
-
-	@RequestMapping("/course/search2.do")
-	public String handleRequest2(
-			@RequestParam("page") String page,
-			@ModelAttribute("tList") List<TasteCategory> tList,
-			@ModelAttribute("cList") PagedListHolder<Course> cList,
-			BindingResult result) throws Exception {
-		if (tList == null || cList == null) {
-			throw new IllegalStateException("Cannot find pre-loaded category and product list");
-		}
-		if ("next".equals(page)) { cList.nextPage(); }
-		else if ("previous".equals(page)) { cList.previousPage(); }
-		return SEARCH_VIEW;
-	}
-	
-	*/
-	
-	/*
-	@ModelAttribute("tasteCate")
-	public TasteCategory[] referencePerformerTypes() {
-		return TasteCategory.values();
-	}
-	
-	@ModelAttribute("tasteCate")
-	protected List<TasteCategory> tasteList() throws Exception {
-		List<TasteCategory> tList = new ArrayList<TasteCategory>();
-		tList.add(new TasteCategory("active"));
-		tList.add(new TasteCategory("calm"));
-		tList.add(new TasteCategory("healing"));
-		tList.add(new TasteCategory("nature"));
-		tList.add(new TasteCategory("official"));
-		tList.add(new TasteCategory("experience"));
-		tList.add(new TasteCategory("entertain"));
-		tList.add(new TasteCategory("retro"));
-		return tList;
-	}*/
-	/*
-	@GetMapping
-	public ModelAndView searchPlaceForm() throws Exception {
-		List<TasteCategory> tList = tasteService.getCategory();
-
-		ModelAndView mav = new ModelAndView(SEARCH_VIEW); // view 이름: "hello" 
-		mav.addObject("tList", tList);
-		return mav;
-	}
-	@PostMapping
-	public ModelAndView submit(HttpServletRequest request,
-			@RequestParam(value="keyword1", required=false) String keyword1,
-			@RequestParam(value="keyword2", required=false) String keyword2,
-			@PageableDefault(page = 0, size = 10, sort = "courseId") Pageable pageable,
-			Model model) throws Exception {
-		ModelAndView mav = new ModelAndView(SEARCH_VIEW); // view 이름: "hello"    
-
-		double x = 37.577552;
-		double y = 126.976869;
-		mav.addObject("x", x);
-		mav.addObject("y", y);
-		String station = "시청역";
-		mav.addObject("station", station);
-		List<Course> cList = searchService.getCourseList(station);
-		mav.addObject("cList", cList);
-		
-		List<TasteCategory> tList = tasteService.getCategory();
-		mav.addObject("tList", tList);
-		
-	   return mav;   
-	}*/
 }
